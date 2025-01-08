7d6ae1c85759a53bbb49f3a4147308c377bdf4d0
Igor <igor.torquato.20@gmail.com>
2025-01-08 19:40:29 -0300

    styles: create global styles

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{ 
+    margin:0;
+    padding:0;
+    font-family: 'Lato',sans-serif;
+    color: #FFFFFF;
+    }
+
+    body{
+    background-color: #081229;
+    }
+`;
+
+export default GlobalStyle;
