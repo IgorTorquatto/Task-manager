7edba3564cd6f0bef6b592cd1221b9069ad96959
Igor <igor.torquato.20@gmail.com>
2025-01-13 16:18:48 -0300

    add: Home page

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,13 +2,14 @@ import { Routes,Route } from 'react-router-dom';
 
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
-
+import { Home } from './pages/Home';
 
 export const Router = () =>{
     return(
         <Routes>
             <Route path='/' element={<Login/>}/>
             <Route path='/register' element={<Register/>}/>
+            <Route path='/home' element={<Home/>}/>
         </Routes>
     )
 }
