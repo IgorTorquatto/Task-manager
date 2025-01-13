88ea3dfd506d21a6243b9a39dbc8921f426b258d
Igor <igor.torquato.20@gmail.com>
2025-01-13 16:17:20 -0300

    style: fixed registration form styles

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -7,10 +7,47 @@ import {
   MDBCardBody,
   MDBInput,
 } from "mdb-react-ui-kit";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import axios from "axios";
 import styles from "./Register.module.css";
 
+//schema de validação
+
+const schema = yup.object().shape({
+  name: yup.string().required("Nome é obrigatório"),
+  email: yup.string().email("E-mail inválido").required("E-mail é obrigatório"),
+  password: yup.string().min(6,"A senha deve ter no mínimo 6 caracteres").required("Senha é obrigatória"),
+  confirmPassword: yup.string().oneOf([yup.ref("password"),null],"As senhas devem ser iguais").required("Confirmação de senha é obrigatória")
+});
+
 export const Register = () => {
+
+  //
+  const navigate = useNavigate();
+  const { register, handleSubmit, formState: {errors},} = useForm({resolver: yupResolver(schema)});
+
+  //Função que envia dados para o back-end
+  const onSubmit = async (data) =>{
+    try{
+      const response = await axios.post("http://localhost:3000/create_user",{
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      });
+
+      if(response.status === 201){
+        navigate("/home");
+      }
+
+    }catch(error){
+      console.log("Erro ao cadastrar usuário", error);
+      alert("Erro ao cadastrar. Por favor, tente novamente.");
+    }
+  }
+
   return (
     <MDBContainer>
       <MDBRow
@@ -22,38 +59,58 @@ export const Register = () => {
               <h2 className="fw-bold text-center">Cadastro</h2>
             </div>
             <MDBCardBody className="p-5 w-100 d-flex flex-column justify-content-start">
-              <MDBInput
-                wrapperClass="mb-4 w-100"
-                label="Nome"
-                id="formControlLg"
-                type="text"
-                size="lg"
-              />
+              <form onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <MDBInput
+                  wrapperClass="w-100"
+                  label="Nome"
+                  id="formControlLg"
+                  type="text"
+                  size="lg"
+                  {...register("name")}
+                />
+                <p className="text-danger">{errors.name?.message}</p>
+              </div>
+              <div>
                <MDBInput
-                wrapperClass="mb-4 w-100"
+                wrapperClass="w-100"
                 label="E-mail"
                 id="formControlLg"
                 type="email"
                 size="lg"
+                {...register("email")}
               />
+              <p className="text-danger">{errors.email?.message}</p>
+              </div>
+              <div>
               <MDBInput
-                wrapperClass="mb-4 w-100"
+                wrapperClass="w-100"
                 label="Senha"
                 id="formControlLg"
                 type="password"
                 size="lg"
+                {...register("password")}
               />
+              <p className="text-danger">{errors.password?.message}</p>
+              </div>
+              <div>
               <MDBInput
-                wrapperClass="mb-4 w-100"
+                wrapperClass="w-100"
                 label="Confirmar senha"
                 id="formControlLg"
                 type="password"
                 size="lg"
+                {...register("confirmPassword")}
               />
+              <p className="text-danger">{errors.confirmPassword?.message}</p>
+              </div>
 
-              <MDBBtn className={styles.registerBtn} size="lg">
+              <div className={`${styles.divRegisterBtn}`}>
+              <MDBBtn className={styles.registerBtn} size="lg" type="submit">
                 Cadastrar
               </MDBBtn>
+              </div>
+              </form>
               <div className={styles.sectionLink}>
                 <p className={styles.registerLink}>
                   Já possui uma conta? <Link to="/">Entrar</Link>
